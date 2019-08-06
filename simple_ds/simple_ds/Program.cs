a5a89a52fe8a89aee2bb0b7ec09661969f6a4891
ArtemkaKun <zumikq78@gmail.com>
2019-08-06 13:31:33 +0300

    [Stack] Added functions: Size(), Reverse(), Copy(), Sort()

--- simple_ds/simple_ds/Program.cs
+++ simple_ds/simple_ds/Program.cs
@@ -7,12 +7,21 @@ namespace simple_ds
         static void Main(string[] args)
         {
             Structures.Stack<int> stack = new Structures.Stack<int>();
+            Structures.Stack<int> stack_queue = new Structures.Stack<int>();
 
             Console.WriteLine(stack.isEmpty());
             stack.Push(1);
             stack.Push(2);
+            stack.Push(6);
+            stack.Push(10);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(9);
+            stack.Push(8);
+            stack.Push(7);
+            stack.Sort();
             Console.WriteLine(stack.Peek());
-            Console.WriteLine(stack.Pop());
         }
     }
 }
--- simple_ds/simple_ds/Structures/Stack.cs
+++ simple_ds/simple_ds/Structures/Stack.cs
@@ -11,14 +11,20 @@ namespace simple_ds.Structures
         //initialization of a stack
         public Stack()
         {
-            this.size_of_stack = 0;
-            this.stack_data = new List<T>();
+            size_of_stack = 0;
+            stack_data = new List<T>();
+        }
+
+        //return size of a stack
+        public int Size()
+        {
+            return size_of_stack;
         }
 
         //check if stack is empty
         public bool isEmpty()
         {
-            if (this.size_of_stack == 0)
+            if (size_of_stack == 0)
             {
                 return true;
             }
@@ -31,7 +37,7 @@ namespace simple_ds.Structures
         // return top element of a stack without remowing it
         public T Peek()
         {
-            if (this.isEmpty())
+            if (isEmpty())
             {
                 throw new Exception("Cannot peek the top element. Stack is empty");
             }
@@ -44,8 +50,8 @@ namespace simple_ds.Structures
         //add new item on top of a stack (NOTE: top of a stack it is actually last element of a list)
         public void Push(T new_item)
         {
-            ++this.size_of_stack;
-            this.stack_data.Add(new_item);
+            ++size_of_stack;
+            stack_data.Add(new_item);
         }
 
         //return top element of a stack and remowe it
@@ -58,9 +64,58 @@ namespace simple_ds.Structures
             else
             {
                 var top_item = this.Peek();              
-                this.stack_data.Remove(top_item);
-                --this.size_of_stack;
+                stack_data.Remove(top_item);
+                --size_of_stack;
                 return top_item;
+            }
+        }
+
+        //return stack with reversed elements order in a stack (turn a stack into a queue)
+        public Stack<T> Reverse()
+        {
+            Stack<T> reversed_stack = new Stack<T>();
+            Stack<T> buffer_stack = new Stack<T>();
+
+            buffer_stack = this.Copy();
+
+            while (!buffer_stack.isEmpty())
+            {
+                reversed_stack.Push(buffer_stack.Pop());
+            }
+
+            return reversed_stack;
+        }
+
+        //return a deep copy of a stack
+        public Stack<T> Copy()
+        {
+            Stack<T> copyed_stack = new Stack<T>();
+
+            for (var element = 0; element < size_of_stack; ++element)
+            {
+                copyed_stack.Push(stack_data[element]);
+            }
+
+            return copyed_stack;
+        }
+
+        //sort values in this stack (bubble sort) TODO
+        public void Sort()
+        {
+            for (var step_by_each_symbol = size_of_stack - 1; step_by_each_symbol > 0; --step_by_each_symbol)
+            {
+                for (var one_symbol = 0; one_symbol < step_by_each_symbol; ++one_symbol)
+                {
+                    
+                    if (Convert.ToInt32(stack_data[one_symbol]) > Convert.ToInt32(stack_data[one_symbol + 1]))
+                    {
+                        T buffer;
+
+                        buffer = stack_data[one_symbol + 1];
+                        stack_data[one_symbol + 1] = stack_data[one_symbol];
+                        stack_data[one_symbol] = buffer;
+                    }
+                }
             }
         }
     }
